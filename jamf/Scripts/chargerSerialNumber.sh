f0261716fdda8252e1f3438bbbe6ac5228ae798c
thameemla <thameemla@gmail.com>
2025-03-19 14:27:07 +0530

    Update chargerSerialNumber.sh

--- jamf/Scripts/chargerSerialNumber.sh
+++ jamf/Scripts/chargerSerialNumber.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#This script fetches the S.No of connected Power Adapter. So we can use this to identify which person is using the adpater through advanced search in Jamf Pro.
+
 # Check if the Mac is charging
 charging_status=$(system_profiler SPPowerDataType | grep -i -m 1 "Charging:" | awk '{print $2}')
 
