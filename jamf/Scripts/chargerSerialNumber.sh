78f5dfe61b9c6198801a97acfdafcd4ae83b4132
thameemla <thameemla@gmail.com>
2025-03-19 14:24:42 +0530

    Update chargerSerialNumber.sh

--- jamf/Scripts/chargerSerialNumber.sh
+++ jamf/Scripts/chargerSerialNumber.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Check if the Mac is charging
 charging_status=$(system_profiler SPPowerDataType | grep -i -m 1 "Charging:" | awk '{print $2}')
@@ -7,11 +7,8 @@ if [[ "$charging_status" == "Yes" ]]; then
     # Extract the charger's serial number
     charger_serial=$(system_profiler SPPowerDataType | awk '/AC Charger Information:/{flag=1} flag && /Serial Number:/{print; exit}' | awk '{print $3}')
     
-    if [[ -n "$charger_serial" ]]; then
-        echo "<result>$charger_serial</result>"
-    else
-        echo "Charger connected, but serial number not found."
-    fi
+    echo "<result>$charger_serial</result>"
+
 else
-    echo "No charger found."
+    echo "<result>No charger found.</result>"
 fi
